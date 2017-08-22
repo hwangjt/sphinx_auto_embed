21d8da6cf2b2061e001478d8d1f04f6ae584bfc6
John Hwang <hwangjt@Johns-MacBook-Pro-2015.local>
2017-08-21 23:24:48 -0400

    Added support for custom directives

--- sphinx_auto_embed/main.py
+++ sphinx_auto_embed/main.py
@@ -1,15 +1,17 @@
+import os
 import sys
 import six
-
-from sphinx_auto_embed.utils import get_rstx_file_paths, get_directives
+from sphinx_auto_embed.utils import get_rstx_file_paths, get_directives, read_embedrc
 
 
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
 
+    custom_directives_dir = read_embedrc()
+
     rstx_file_paths_list = get_rstx_file_paths()
-    directives_list = get_directives()
+    directives_list = get_directives(custom_directives_dir)
 
     for file_dir, file_name in rstx_file_paths_list:
         file_path = file_dir + '/' + file_name
--- sphinx_auto_embed/utils.py
+++ sphinx_auto_embed/utils.py
@@ -1,7 +1,38 @@
 import os
+import sys
 import importlib
 import pkgutil
 import inspect
+
+
+def read_embedrc():
+    # Keep going up a directory until we find '.embedrc' or go all the way to the top
+    orig_cwd = os.getcwd()
+    cwd = os.getcwd()
+    while not os.path.isfile(cwd + '/.embedrc'):
+        os.chdir("..")
+        new_cwd = os.getcwd()
+        if new_cwd == cwd:
+            break
+        else:
+            cwd = new_cwd
+    embedrc_dir = cwd
+
+    custom_directives_dir = None
+
+    # If there is an '.embedrc':
+    if os.path.isfile(embedrc_dir + '/.embedrc'):
+        # Load the file
+        with open(embedrc_dir + '/.embedrc', 'r') as f:
+            lines = f.readlines()
+
+        # Read in custom_directives_dir
+        for iline, line in enumerate(lines):
+            stripped_line = line.replace(' ', '')
+            if stripped_line[:22] == 'custom_directives_dir=':
+                custom_directives_dir = embedrc_dir + '/' + stripped_line[22:]
+
+    return custom_directives_dir
 
 
 def get_rstx_file_paths():
@@ -24,9 +55,14 @@ def get_rstx_file_paths():
     return rstx_file_paths_list
 
 
-def get_directives():
+def get_directives(custom_directives_dir):
     """
     Get a list of custom directives that ship with sphinx_auto_embed.
+
+    Parameters
+    ----------
+    custom_directives_dir : str
+        Absolute path to the directory containing custom directives for the current project.
 
     Returns
     -------
@@ -46,5 +82,17 @@ def get_directives():
                 if name[:9] == 'Directive':
                     if inspect.isclass(obj) and hasattr(obj, 'NAME') and hasattr(obj, 'NUM_ARGS'):
                         directives.append(obj())
+
+    if custom_directives_dir is not None:
+        for file_dir, dirs, files in os.walk(custom_directives_dir):
+            for file_name in files:
+                if file_name[:9] == 'directive' and file_name[-3:] == '.py':
+                    sys.path.append(file_dir)
+                    module = importlib.import_module(file_name[:-3])
+
+                    for name, obj in inspect.getmembers(module):
+                        if name[:9] == 'Directive':
+                            if inspect.isclass(obj) and hasattr(obj, 'NAME') and hasattr(obj, 'NUM_ARGS'):
+                                directives.append(obj())
 
     return directives
