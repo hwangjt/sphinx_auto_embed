a3cf8e7396abd8e8b59b34411a99658d616ade5c
John Hwang <hwangjt@Johns-MacBook-Pro-2015.local>
2017-08-30 16:58:15 -0400

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from distutils.core import setup
 
-setup(name='sphinx_auto_embed',
-      version='0.1',
-      description="A utility for generating rst files with automatically embedded code, code output, and other data for documentation.",
-      license='Apache-2.0',
-      install_requires=[
+setup(
+    name='sphinx_auto_embed',
+    version='0.1',
+    description="A utility for generating rst files with automatically embedded code, code output, and other data for documentation.",
+    license='Apache-2.0',
+    install_requires=[
         'six',
-      ],
-      packages=['sphinx_auto_embed'],
-      entry_points="""
-          [console_scripts]
-          sphinx_auto_embed=sphinx_auto_embed.main:main
-      """
-      )
+    ],
+    packages=[
+        'sphinx_auto_embed',
+        'sphinx_auto_embed.directives',
+    ],
+    entry_points="""
+        [console_scripts]
+        sphinx_auto_embed=sphinx_auto_embed.main:main
+    """
+    )
